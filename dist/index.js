9f46bb2f286dd9160925cdc8e0996e71af3b1d0f
Juan Gilsanz Polo <juangilsanz@gmail.com>
2023-03-08 15:35:23 +0100

    Changed startup error

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./server"));
-server_1.default.listen(server_1.default.get('port'), () => {
-    console.log(`Server listening on ${server_1.default.get('port')}`);
-});
+server_1.default
+    .listen(server_1.default.get("port"))
+    .on("listening", () => console.log("Server listening on port", server_1.default.get("port")))
+    .on("error", (e) => console.log(e));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import server from './server';
+import server from "./server";
 
-server.listen(server.get('port'), () => {
-  console.log(`Server listening on ${server.get('port')}`)
-});
+server
+  .listen(server.get("port"))
+  .on("listening", () =>
+    console.log("Server listening on port", server.get("port"))
+  )
+  .on("error", (e) => console.log(e));
