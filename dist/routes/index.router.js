538e5d7773b1dbbe9ada18cdab3a67ea3973e100
Juan Gilsanz Polo <juangilsanz@gmail.com>
2023-03-09 00:40:48 +0100

    Removed dist folder

--- dist/routes/index.router.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-exports.default = router;
