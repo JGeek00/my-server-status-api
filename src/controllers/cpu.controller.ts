535e79ed6b80c8f64d5eb1732f426f4d75e2b35a
Juan Gilsanz Polo <juangilsanz@gmail.com>
2023-03-12 18:41:48 +0100

    Added cpu temperature on cpu endpoint

--- src/controllers/cpu.controller.ts
+++ src/controllers/cpu.controller.ts
@@ -2,10 +2,20 @@ import si, { Systeminformation } from "systeminformation";
 import dataConfig from "../config/data.json";
 
 const getCpuInfo = async () => {
-  const { cpu }: { cpu: Systeminformation.CpuData } = await si.get({
-    cpu: dataConfig.cpu.join(","),
+  const {
+    cpu,
+    cpuCurrentSpeed,
+    cpuTemperature,
+  }: {
+    cpu: Systeminformation.CpuData;
+    cpuCurrentSpeed: Systeminformation.CpuCurrentSpeedData;
+    cpuTemperature: Systeminformation.CpuTemperatureData;
+  } = await si.get({
+    cpu: dataConfig.cpu.cpu.join(","),
+    cpuCurrentSpeed: dataConfig.cpu.currentSpeed.join(","),
+    cpuTemperature: dataConfig.cpu.temperature.join(","),
   });
-  return cpu;
+  return { cpu, cpuCurrentSpeed, cpuTemperature };
 };
 
 export default getCpuInfo;
