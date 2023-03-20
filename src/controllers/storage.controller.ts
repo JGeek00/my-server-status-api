e351a7efb32c99b263af388963bf60cb95193ce3
Juan Gilsanz Polo <juangilsanz@gmail.com>
2023-03-21 00:06:36 +0100

    Added more information storage

--- src/controllers/storage.controller.ts
+++ src/controllers/storage.controller.ts
@@ -4,17 +4,21 @@ import dataConfig from "../config/data.json";
 const getStorageInfo = async () => {
   const {
     diskLayout,
+    blockDevices,
     fsSize,
   }: {
-    diskLayout: Systeminformation.DiskLayoutData;
+    diskLayout: Systeminformation.DiskLayoutData
+    blockDevices: Systeminformation.BlockDevicesData;
     fsSize: Systeminformation.FsSizeData;
   } = await si.get({
     diskLayout: dataConfig.storage.diskLayout.join(","),
+    blockDevices: dataConfig.storage.blockDevices.join(","),
     fsSize: dataConfig.storage.fsSize.join(","),
   });
-  
+
   return {
     diskLayout,
+    blockDevices,
     fsSize
   }
 };
