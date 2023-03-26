86eb8258d66cc5159e9c18dffa2ff574fe2a146c
Juan Gilsanz Polo <juangilsanz@gmail.com>
2023-03-26 04:57:37 +0200

    Added more info status

--- src/controllers/status.controller.ts
+++ src/controllers/status.controller.ts
@@ -3,25 +3,31 @@ import data from "../config/data.json";
 
 const getStatus = async () => {
   const {
+    cpu,
     cpuCurrentSpeed,
     cpuTemperature,
     mem,
+    memLayout,
     fsStats,
     currentLoad,
     networkStats,
     osInfo,
   }: {
+    cpu: Systeminformation.CpuData;
     cpuCurrentSpeed: Systeminformation.CpuTemperatureData;
     cpuTemperature: Systeminformation.CpuTemperatureData;
     mem: Systeminformation.MemData;
+    memLayout: Systeminformation.MemLayoutData[];
     fsStats: Systeminformation.FsStatsData;
     currentLoad: Systeminformation.CurrentLoadData;
     networkStats: Systeminformation.NetworkStatsData;
     osInfo: Systeminformation.OsData;
   } = await si.get({
+    cpu: data.status.cpuInfo.join(","),
     cpuCurrentSpeed: data.status.cpuSpeed.join(","),
     cpuTemperature: data.status.cpuTemperature.join(","),
     mem: data.status.memory.join(","),
+    memLayout: data.status.memoryInfo.join(","),
     fsStats: data.status.storage.join(","),
     currentLoad: data.status.load.join(","),
     networkStats: data.status.network.join(","),
@@ -29,6 +35,12 @@ const getStatus = async () => {
   });
 
   const cpuStatus = {
+    specs: {
+      "name": `${cpu.manufacturer} ${cpu.brand}`,
+      "minSpeed": cpu.speedMin,
+      "speed": cpu.speed,
+      "maxSpeed": cpu.speedMax
+    },
     cores: Array.from({ length: cpuCurrentSpeed.cores.length }, (_, i) => ({
       speed: cpuCurrentSpeed.cores[i] ?? null,
       temperature: cpuTemperature.cores[i] ?? null,
@@ -47,6 +59,10 @@ const getStatus = async () => {
   };
 
   const memory = {
+    specs: {
+      "capacity": memLayout.map(m => m.size).reduce((a, b) => a + b),
+      "type": memLayout[0].type
+    },
     total: mem.total ?? null,
     used: mem.used ?? null,
     free: mem.free ?? null,
